595351aab1db86ba4cabfc04621116c7ba380cc6
4lbert4rgolo <albertsilvadeargolo@gmail.com>
2026-03-26 16:15:44 -0300

    Atualizando a view home

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Task;
+
 class TaskController extends Controller
     {
         public function index(){
 
-            return view('welcome');
+            $tasks = Task::all();
+
+            return view('welcome', ['tasks'=> $tasks]);
         }  
         
         public function create(){
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,28 @@
 
 @section('content')
 
-
-
-<body>
-    <h1>Página inicial</h1>
-    <img src="/img/helpdesk.jpeg" alt="Banner">
-</body>
-
+<div id="search-container" class="col-md-12">
+    <h1>Busque uma Tarefa</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="procurar">
+    </form>
+</div>
+<div id="tasks-container" class="col-md-12">
+    <h2>Tarefas em andamentos</h2>
+    <p class="subtitle">Verifique as tarefas que ainda não foram finalizadas</p>
+    <div id="card-container" class="row">
+        @foreach($tasks as $tasks)
+        <div class="card col-md-3">
+            <img src="img\placeholder.png" alt="{{ $task->title}}">
+            <div class="card-body">
+                <p class="card-date">10/04/2026</p>
+                <h5 class="card-title">{{ $event->title }}</h5>
+                <p class="card-responsible">Responsável</p>
+                <a href="#" class="btn btn-primary">Visualizar</a>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+  
 @endsection
